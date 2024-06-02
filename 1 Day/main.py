a9167416d8c69a3343bb18da1b90c4b3904c90ed
Arunesh <aruneshkumar2026@gmail.com>
2024-06-02 06:15:43 +0530

    1 - Day

---
+++ 1 Day/main.py
@@ -0,0 +1,11 @@
+print("Hello, Arunesh")
+
+print("Hello\tArunesh")
+print("Hello\nArunesh")
+
+print("Hello" + " " + "Arunesh")
+
+first_name = input("Enter your firstname:\n")
+last_name = input("Enter your lastname:\n")
+full_name = first_name + " " + last_name
+print("Hello, " + full_name)
