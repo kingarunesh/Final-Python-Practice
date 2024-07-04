89b352774e27b6a8b24e3fb4c6e446506a59d915
Arunesh <aruneshkumar2026@gmail.com>
2024-07-04 19:33:37 +0530

    21.4 : increase snake

--- 20 Day/main.py
+++ 20 Day/main.py
@@ -52,6 +52,7 @@ while game_is_on:
     #!      detect collision with food
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extend_snake()
         scoreboard.increment_score()
     
     #!      delect wall collision
