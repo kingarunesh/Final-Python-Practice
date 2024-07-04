c1137be62ff29e93c377389a9ea199cb90ac049b
Arunesh <aruneshkumar2026@gmail.com>
2024-07-04 18:31:58 +0530

    21.1 : detect food

--- 20 Day/main.py
+++ 20 Day/main.py
@@ -1,7 +1,8 @@
 from turtle import Turtle, Screen
 import time
-from snake import Snake
 
+from snake import Snake
+from food import Food
 
 
 #NOTE :             screen
@@ -15,6 +16,9 @@ screen.tracer(0)
 
 #!          snake class
 snake = Snake()
+
+#!          food
+food = Food()
 
 
 #!          keyboard listen
@@ -33,9 +37,14 @@ screen.onkey(fun=snake.right, key="Right")
 game_is_on = True
 while game_is_on:
     screen.update()
-    time.sleep(0.1)
+    time.sleep(0.2)
     
+    #!      move snake
     snake.move()
+    
+    #!      detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 
 screen.exitonclick()
