5ca947d73e4ec115a8e5e330cbfdf78419205c58
Arunesh <aruneshkumar2026@gmail.com>
2024-07-04 19:37:48 +0530

    21.5 : tail touch detect

--- 20 Day/main.py
+++ 20 Day/main.py
@@ -59,6 +59,14 @@ while game_is_on:
     if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
         game_is_on = False
         scoreboard.game_over()
+    
+    #!      detect snake tail touch
+    for segment in snake.segements:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 screen.exitonclick()
