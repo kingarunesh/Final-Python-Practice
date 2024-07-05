3a3e1c8168dde6c213b86981cbb81da029a9d5ee
Arunesh <aruneshkumar2026@gmail.com>
2024-07-05 15:17:34 +0530

    22.2 : right | left - paddle

--- 22 Day/main.py
+++ 22 Day/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+from paddle import Paddle
 
 
 
@@ -14,21 +15,15 @@ screen.tracer(0)
 
 #NOTE :         paddle
 
-def go_up():
-    right_paddle.goto(x=right_paddle.xcor(), y=right_paddle.ycor() + 20)
 
-def go_down():
-    right_paddle.goto(x=right_paddle.xcor(), y=right_paddle.ycor() - 20)
-
-right_paddle = Turtle(shape="square")
-right_paddle.shapesize(stretch_len=1, stretch_wid=5)
-right_paddle.color("white")
-right_paddle.penup()
-right_paddle.goto(x=350, y=0)
+r_paddle = Paddle(position=(350, 0))
+l_paddle = Paddle(position=(-350, 0))
 
 
-screen.onkey(fun=go_up, key="Up")
-screen.onkey(fun=go_down, key="Down")
+screen.onkey(fun=r_paddle.go_up, key="Up")
+screen.onkey(fun=r_paddle.go_down, key="Down")
+screen.onkey(fun=l_paddle.go_up, key="w")
+screen.onkey(fun=l_paddle.go_down, key="s")
 
 
 game_is_on = True
