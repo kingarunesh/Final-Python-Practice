958ac13369bebbeaa50d3573cd30ad36632ceee2
Arunesh <aruneshkumar2026@gmail.com>
2024-07-05 15:28:21 +0530

    22.3 : ball move

--- 22 Day/main.py
+++ 22 Day/main.py
@@ -1,5 +1,7 @@
 from turtle import Turtle, Screen
 from paddle import Paddle
+from ball import Ball
+import time
 
 
 
@@ -25,10 +27,15 @@ screen.onkey(fun=r_paddle.go_down, key="Down")
 screen.onkey(fun=l_paddle.go_up, key="w")
 screen.onkey(fun=l_paddle.go_down, key="s")
 
+#NOTE :         ball
+ball = Ball()
+
 
 game_is_on = True
 while game_is_on:
+    time.sleep(0.1)
     screen.update()
+    ball.move()
 
 
 screen.exitonclick()
