b2dc9613947a0a154e83600e36c8f23d4516e9e7
Arunesh <aruneshkumar2026@gmail.com>
2024-07-06 17:16:09 +0530

    22.5 : reset game on ball missed

--- 22 Day/main.py
+++ 22 Day/main.py
@@ -44,6 +44,14 @@ while game_is_on:
     #!      detect collision with paddle
     if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
         ball.bounce_x()
+    
+    #!      detect r_paddle misses ball
+    if ball.xcor() > 380:
+        ball.reset_position()
+    
+    #!      detect l_paddle misses ball
+    if ball.xcor() < -380:
+        ball.reset_position()
 
 
 screen.exitonclick()
