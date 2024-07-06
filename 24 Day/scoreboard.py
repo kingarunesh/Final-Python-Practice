32a9b56ab99b0185c05f57e7ab7966df756c8c72
Arunesh <aruneshkumar2026@gmail.com>
2024-07-06 23:57:31 +0530

    24.2 : high score added

--- 24 Day/scoreboard.py
+++ 24 Day/scoreboard.py
@@ -1,6 +1,6 @@
 from turtle import Turtle
 ALIGNMENT = "center"
-FONT = ("Courier", 24, "normal")
+FONT = ("Courier", 18, "normal")
 
 
 class Scoreboard(Turtle):
@@ -8,6 +8,7 @@ class Scoreboard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.high_score = 0
         self.color("white")
         self.penup()
         self.goto(0, 260)
@@ -15,13 +16,15 @@ class Scoreboard(Turtle):
         self.update_scoreboard()
 
     def update_scoreboard(self):
-        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
-
-    def game_over(self):
-        self.goto(0, 0)
-        self.write("GAME OVER", align=ALIGNMENT, font=FONT)
+        self.clear()
+        self.write(f"Score: {self.score} | Hight Score: {self.high_score}", align=ALIGNMENT, font=FONT)
+    
+    def game_reset(self):
+        if self.score > self.high_score:
+            self.high_score = self.score
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
         self.update_scoreboard()
