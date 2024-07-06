d126f9a1833e2022cafa4d3aca67ae042900535d
Arunesh <aruneshkumar2026@gmail.com>
2024-07-06 17:12:57 +0530

    22.4 : ball bounce

--- 22 Day/ball.py
+++ 22 Day/ball.py
@@ -7,6 +7,14 @@ class Ball(Turtle):
         self.shape("circle")
         self.color("white")
         self.penup()
+        self.x_move = 10
+        self.y_move = 10
     
     def move(self):
-        self.goto(x=self.xcor() + 10, y=self.ycor() + 10)
+        self.goto(x=self.xcor() + self.x_move, y=self.ycor() + self.y_move)
+    
+    def bounce_y(self):
+        self.y_move *= -1
+    
+    def bounce_x(self):
+        self.x_move *= -1
--- 22 Day/main.py
+++ 22 Day/main.py
@@ -1,4 +1,4 @@
-from turtle import Turtle, Screen
+from turtle import Screen
 from paddle import Paddle
 from ball import Ball
 import time
@@ -36,6 +36,14 @@ while game_is_on:
     time.sleep(0.1)
     screen.update()
     ball.move()
+    
+    #!      detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+    
+    #!      detect collision with paddle
+    if ball.distance(r_paddle) < 50 and ball.xcor() > 320 or ball.distance(l_paddle) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
 
 
 screen.exitonclick()
