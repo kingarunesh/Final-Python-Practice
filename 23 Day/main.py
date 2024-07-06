29625d2d99bfa47e41d966db2d8fb416bd616e33
Arunesh <aruneshkumar2026@gmail.com>
2024-07-06 19:54:59 +0530

    23.7 : score board

--- 23 Day/main.py
+++ 23 Day/main.py
@@ -31,6 +31,9 @@ screen.onkey(fun=player.go_up, key="Up")
 car_manager = CarManager()
 
 
+scoreboard = Scoreboard()
+
+
 game_is_on = True
 while game_is_on:
     time.sleep(0.1)
@@ -43,11 +46,13 @@ while game_is_on:
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.game_over()
     
     #!      detect successfull crossing
     if player.is_at_finish_line():
         player.go_to_start()
         car_manager.level_up()
+        scoreboard.increase_level()
 
 
 screen.exitonclick()
--- 23 Day/scoreboard.py
+++ 23 Day/scoreboard.py
@@ -1,5 +1,26 @@
-FONT = ("Courier", 24, "normal")
+from turtle import Turtle
 
 
-class Scoreboard:
-    pass
+FONT = ("Courier", 24, "bold")
+
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.penup()
+        self.level = 1
+        self.goto(x=-280, y=250)
+        self.update_scoreboard()
+        
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Level: {self.level}", align="left", font=FONT)
+        
+    def increase_level(self):
+        self.level += 1
+        self.update_scoreboard()
+    
+    def game_over(self):
+        self.goto(0, 0)
+        self.write("Game Over", align="center", font=FONT)
